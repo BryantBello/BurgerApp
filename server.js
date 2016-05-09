9e347e3759ff2e12920e2420c991ce099cf96f86
bryantbello <bryant.bello@outlook.com>
2016-05-09 00:46:59 -0400

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
-var PORT = process.env.PORT || 3008;
+var PORT = process.env.PORT || 3018;
 
 
 var app = express();
